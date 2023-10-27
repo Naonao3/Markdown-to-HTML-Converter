4ea2b75df69e49b7a2e93dcc3084f00de7ae912b
Naoto Nishimura <naoto.n.2460@gmail.com>
2023-10-28 04:00:29 +0900

    first commit

---
+++ file-converter.py
@@ -0,0 +1,23 @@
+import sys
+import markdown
+
+def main():
+    command = sys.argv[1]
+    inputFile = sys.argv[2]
+    outputFile = sys.argv[3]
+
+    if command == "markdown":
+        try:
+            with open(inputFile,"r") as f:
+                text = f.read()
+        except FileExistsError:
+            print("no file")
+        md = markdown.Markdown(extensions=["toc", "sane-lists"])
+        html = md.convert(text)
+        with open(outputFile,"w") as f:
+            f.write(html)
+    else:
+        ("input correct command")
+
+if __name__ == "__main__":
+    main()
